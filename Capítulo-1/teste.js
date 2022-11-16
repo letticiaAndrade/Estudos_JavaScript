4608194f5afca4317112d7103bfadcbc88d6616b
Leticia Andrade <leticiaae@hotmail.com.br>
2022-11-16 15:11:21 -0300

    Estrutura básica de JavaScript variáveis

---
+++ Capítulo-1/teste.js
@@ -0,0 +1,49 @@
+
+// declaração de variáveis
+var x;    // declara uma variável chamada x
+
+// valores podem ser atribuídos às variáveis com o sinal =
+x = 0;    // agira a variável x tem o valor 0
+x         // => =: uma varíavel é avaliada com seu valor 
+
+// JavaScript aceita vários tipos de valores 
+
+x = 1;                  // números
+x = 0.01;               // Apenas um tipo Number para inteiros e reais
+x = "Hello World";      // strings de texto entre aspas
+x = 'JavaScript';       //  apóstrofos também delimitam string
+x = true;               // valores booleanos 
+x = false;              // o outro valor booleano
+x = null;               // null é um valor especial que significa "nenhum valor"
+x = undefined;          // undefined é como null
+
+
+// declaração de um objeto 
+var boneco = {                  // objetos são colocados entre chavas
+    topic: "JavaScript",        // a propriedade "topic" tem o valor "JavaScript"
+    fat: true                   // a propriedade "fat" tem o valor "JavaScript"
+};                              // a chava marca o fim do objeto
+
+// acesse as propriedades de um objeto com . ou []:
+
+boneco.fat                      // => "JavaScript"
+book ["fat"]                    // => true: outro modo de acessar valores de propriedade.
+book.author = "Flanagan";       // crie novas por meio de atribuição
+book.contents = {};
+
+// JavaScript também aceita arrays (listas indexadas numericamente) de valores.
+var primes = [2, 3, 5, 7];      // um array de 4 valores, delimitados com [ e ].
+primes [0]                      // => 2: o primeiro elemento (índice 0) do array.
+primes.length                   // => 4: quantidade de elementos no array.
+primes [primes.length-1]        // =7: o último elemento do array.
+primes [4] = 9;                 // adiciona um novo elemento por meio de atribuição
+primes [4] = 11;                // ou altera um elemento existente por meio de atribuição
+var empty = []                  // [] é um array vazio, sem quarquer elemento
+empty.length                    // => 0
+
+// Os arrays e objetos podem conter outros arrays e objetos: 
+var points = [
+    {}
+]
+
+    
