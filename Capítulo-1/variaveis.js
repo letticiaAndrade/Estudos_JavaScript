1b4dd04833566148b4641a4a8dd723f11dc71039
Leticia Andrade <leticiaae@hotmail.com.br>
2022-12-19 08:55:27 -0300

    adicionais

--- Capítulo-1/variaveis.js
+++ Capítulo-1/variaveis.js
@@ -159,3 +159,26 @@ function abs(x) { // Uma função para calcular o valor absoluto
     return product; // Retorna o fatorial
    }
    factorial2(5) // => 120: 1*2*3*4*5
+
+
+   // Define uma função construtora para inicializar um novo objeto Point
+function Point(x,y) { // Por convenção, as construtoras começam com letras
+    // maiúsculas
+    this.x = x; // A palavra-chave this é o novo objeto que está sendo
+    // inicializado
+    this.y = y; // Armazena os argumentos da função como propriedades do
+    // objeto
+   } // Nenhum return é necessário
+   // Usa uma função construtora com a palavra-chave "new" para criar instâncias
+   var p = new Point(1, 1); // O ponto geométrico (1,1)
+
+   // Define métodos para objetos Point atribuindo-os ao objeto
+   // prototype associado à função construtora.
+   Point.prototype.r = function() {
+    return Math.sqrt( // Retorna a raiz quadrada de x2+ y2
+    this.x * this.x + // Este é o objeto Point no qual o método...
+    this.y * this.y //...é chamado.
+    );
+   };
+   // Agora o objeto Point b (e todos os futuros objetos Point) herda o método r()
+   p.r() // => 1,414...
